4a8d90a956be2cafb0389989bcc8c737f3147a36
Vshal8 <96942419+Vshal8@users.noreply.github.com>
2023-08-18 21:42:29 +0530

    Setup Tanstack Query and layout.js

---
+++ app/_layout.js
@@ -0,0 +1,21 @@
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+import { Slot } from 'expo-router';
+
+const queryClient = new QueryClient()
+
+
+export default function RootLayout() {
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                    <Slot />
+            </QueryClientProvider>
+        </>
+    );
+}
--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
-import { Text } from 'react-native';
+import {  Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import RootLayout from './_layout';
 
 export default function Page() {
-  return <Text>My First Page</Text>;
+    return <SafeAreaView style={{ flex: 1 }}>
+        <Text>My First Page</Text>
+    </SafeAreaView>
 }
